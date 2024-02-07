6aafeb4976ab0e2a941c794ff4eed90ee848b12f
mathu15 <mathumitha1509@gmail.com>
2024-02-07 11:27:52 +0530

    Login,Signup,Verification UI

---
+++ client/app/layout.tsx
@@ -0,0 +1,36 @@
+import { Poppins } from "next/font/google";
+import { Josefin_Sans } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "./utils/theme-provider";
+import { Toaster } from "react-hot-toast";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-Poppins",
+});
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-Josefin",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`!bg-white bg-no-repeat dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300 ${poppins.variable} ${josefin.variable}`}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+          <Toaster position="top-center" reverseOrder={false} />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ client/app/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React, { FC, useState } from "react";
+import Heading from "./utils/Heading";
+import Header from "./components/Header";
+import Hero from "./components/Route/Hero";
+
+interface Props {}
+
+const Page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
+  const [route, setRoute] = useState("Login");
+  return (
+    <div>
+      <Heading
+        title="Chainscript"
+        description="platform to learn"
+        keywords="blockchain,bsv,programming"
+      />
+      <Header
+        open={open}
+        setOpen={setOpen}
+        activeItem={activeItem}
+        route={route}
+        setRoute={setRoute}
+      />
+      <Hero />
+    </div>
+  );
+};
+export default Page;
